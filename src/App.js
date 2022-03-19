f294fa1da424d1a0318c03f5f4ef9410e600c85e
Eriko <eriko.918k@gmail.com>
2022-03-19 21:33:04 +0900

    Footer editted

--- src/App.js
+++ src/App.js
@@ -6,15 +6,23 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header"> 
-          <img src={logo} className="App-logo img-fluid"
-            alt="logo" />
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
         </header>
         <main>
           <Dictionary defaultKeyword="sunset" />
         </main>
         <footer className="App-footer">
-          <small>Coded by Me</small>
+          <small>
+            Coded by Eriko and is open-sourced on 
+            <a href="https://github.com/eriko1819/dictionary-project"
+             taret="_blank" rel="noreferrer">
+            Github
+            </a> and hosted on 
+            <a href="https://vigilant-agnesi-002a1e.netlify.app/"
+             target="_blank" rel="noreferr">
+            Netlify </a>
+          </small>
         </footer>
       </div>
     </div>
